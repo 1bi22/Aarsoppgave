985fcfbc443034a14f4f6a391c62b3a71ce0263a
1bi22 <Raspoeiraibi@gmail.com>
2024-05-16 13:20:51 +0200

    first commit

---
+++ TerminOppgave/tetris.php
@@ -0,0 +1,17 @@
+<?php
+include "db_connect.php";
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+<body>
+<canvas id="tetrisBoard"></canvas>
+<script src="Script.js"></script>
+</body>
+</html>
